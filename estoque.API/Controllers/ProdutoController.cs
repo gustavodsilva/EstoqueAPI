90169c4a2e8e96fd32c46492e45f4c33ca9f55aa
gustavodsilva <gustavo.gsilva83@gmail.com>
2026-04-24 15:43:47 -0300

    Adiciona ProdutoController e ProdutoService à API
    Implementa ProdutoController com endpoints para listar e adicionar produtos. Cria ProdutoService para gerenciar produtos em memória. Registra ProdutoService como singleton no Program.cs. Remove referências a pastas Controllers e Services no estoque.API.csproj.

---
+++ estoque.API/Controllers/ProdutoController.cs
@@ -0,0 +1,29 @@
+﻿namespace estoque.API.Controllers;
+using estoque.API.Models;
+using estoque.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProdutoController : ControllerBase
+{
+    private readonly ProdutoService _service;
+
+    public ProdutoController(ProdutoService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public IActionResult Listar()
+    {
+        return Ok(_service.Listar());
+    }
+
+    [HttpPost]
+    public IActionResult Adicionar([FromBody] Produto produto)
+    {
+        _service.Adicionar(produto);
+        return Ok("Produto adicionado com sucesso");
+    }
+}
---
+++ estoque.API/Services/ProdutoService.cs
@@ -0,0 +1,16 @@
+﻿namespace estoque.API.Services;
+using estoque.API.Models;
+
+public class ProdutoService
+{
+    private List<Produto> produtos = new List<Produto>();
+    public List<Produto> Listar() => produtos;
+    public void Adicionar(Produto produto)
+    {
+        produtos.Add(produto);
+    }
+    public Produto? BuscarPorNome(string nome)
+    {
+        return produtos.FirstOrDefault(p => p.Nome == nome);
+    }
+}
